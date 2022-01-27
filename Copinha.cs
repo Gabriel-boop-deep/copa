6b8ef77a11e1b9fbe505a813e716500755de0bf7
Gabriel-boop-deep <72049404+Gabriel-boop-deep@users.noreply.github.com>
2022-01-27 18:55:38 -0300

    Copa mundial
    Trata-se de uma possível ocorrência de copama partir de uma entrada do tipo numérica.

---
+++ Copinha.cs
@@ -0,0 +1,31 @@
+using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+     string str;
+            while((str=Console.ReadLine())!= null)
+            {
+                if (int.Parse(str)==0 )
+                {
+                  Console.WriteLine("vai ter copa!");
+                       
+                }
+                else          //Escreva o seu código nos espaços em branco
+                { 
+                    int x = int.Parse(str);
+                    if (  x==0       )
+                    {
+                    Console.WriteLine( "vai ter copa!"  ); 
+                    }
+                    else
+                    {
+                        Console.WriteLine( "vai ter duas!" );
+                    }
+                }
+            }
+
+    }
+
+}
